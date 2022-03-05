f1fa9969ff3b2d6ccc210383f4c72566831ac716
harveyaa <annabelle.ahrv@gmail.com>
2022-03-04 19:28:31 -0500

    deepened decoder

--- models.py
+++ models.py
@@ -56,13 +56,26 @@ class encoder(nn.Module):
 class head(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc3 = nn.Linear(64,2)
+        #self.fc1 = nn.Linear(256*52, 64)
+        #self.batch1 = nn.BatchNorm1d(64)
+        #self.fc2 = nn.Linear(64, 64)
+        #self.batch2 = nn.BatchNorm1d(64)
+        self.fc3 = nn.Linear(64,64)
+        self.batch3 = nn.BatchNorm1d(64)
+        self.fc4 = nn.Linear(64,2)
 
         self.dropout = nn.Dropout()
         self.softmax = nn.Softmax(dim=1)
     
     def forward(self,x):
-        x = self.softmax(self.fc3(x))
+        #x = x.view(x.size()[0],-1)
+        #x = self.dropout(F.relu(self.fc1(x)))
+        #x = self.batch1(x)
+        #x = self.dropout(F.relu(self.fc2(x)))
+        #x = self.batch2(x)
+        x = self.dropout(F.relu(self.fc3(x)))
+        x = self.batch3(x)
+        x = self.softmax(self.fc4(x))
         return x
 
 class HPSModel(nn.Module):
