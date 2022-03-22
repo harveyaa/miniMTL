bd610b03ebc9307c1ed11ed04317e47f43009133
harveyaa <annabelle.ahrv@gmail.com>
2022-03-22 15:39:58 -0400

    added architecture search script

---
+++ examples/arch_search/local_singles.sh
@@ -0,0 +1,12 @@
+python ../n_task.py --tasks SZ --num_epochs 50
+python ../n_task.py --tasks ASD --num_epochs 50
+python ../n_task.py --tasks BIP --num_epochs 50
+
+python ../n_task.py --tasks DEL22q11_2 --num_epochs 50
+python ../n_task.py --tasks DUP22q11_2 --num_epochs 50
+
+python ../n_task.py --tasks DEL16p11_2 --num_epochs 50
+python ../n_task.py --tasks DUP16p11_2 --num_epochs 50
+
+python ../n_task.py --tasks DEL1q21_1 --num_epochs 50
+python ../n_task.py --tasks DUP1q21_1 --num_epochs 50
---
+++ examples/n_task.py
@@ -0,0 +1,81 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+
+from miniMTL.datasets import caseControlDataset
+from miniMTL.models import *
+from miniMTL.training import Trainer
+from miniMTL.util import split_data
+
+from argparse import ArgumentParser
+
+""" 
+n_task
+------
+March 22 2022
+    Script to run HPS model on n_tasks (destined for architecture search).
+"""
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--tasks",help="tasks",dest='tasks',nargs='*')
+    parser.add_argument("--data_dir",help="path to data dir",dest='data_dir',
+                        default='/home/harveyaa/Documents/fMRI/data/ukbb_9cohorts/')
+    parser.add_argument("--log_dir",help="path to log_dir",dest='log_dir',default=None)
+    parser.add_argument("--batch_size",help="batch size for training/test loaders",default=16,type=int)
+    parser.add_argument("--lr",help="learning rate for training",default=1e-3,type=float)
+    parser.add_argument("--num_epochs",help="num_epochs for training",default=100,type=int)
+    args = parser.parse_args()
+    
+    print('#############\n# HPS model #\n#############')
+    print('Tasks: ',args.tasks)
+    print('Batch size: ',args.batch_size)
+    print('LR: ', args.lr)
+    print('Epochs: ', args.num_epochs)
+    print('#############\n')
+
+    # Define paths to data
+    p_pheno = os.path.join(args.data_dir,'pheno_01-12-21.csv')
+    p_conn = os.path.join(args.data_dir,'connectomes')
+
+    # Create datasets
+    print('Creating datasets...')
+    cases = args.tasks
+    data = []
+    for case in cases:
+        print(case)
+        data.append(caseControlDataset(case,p_pheno,p_conn))
+    print('Done!\n')
+    
+    # Split data & create loaders & loss fns
+    loss_fns = {}
+    trainloaders = {}
+    testloaders = {}
+    decoders = {}
+    for d, case in zip(data,cases):
+        train_d, test_d = split_data(d)
+
+        trainloaders[case] = DataLoader(train_d, batch_size=args.batch_size, shuffle=True)
+        testloaders[case] = DataLoader(test_d, batch_size=args.batch_size, shuffle=True)
+        loss_fns[case] = nn.CrossEntropyLoss()
+        decoders[case] = head().double()
+    
+    # Create model
+    model = HPSModel(encoder().double(),
+                decoders,
+                loss_fns)
+    
+    # Create optimizer & trainer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    trainer = Trainer(optimizer,num_epochs=args.num_epochs)
+
+    # Train model
+    trainer.fit(model,trainloaders,testloaders)
+
+    # Evaluate at end
+    metrics = model.score(testloaders)
+    for key in metrics.keys():
+        print()
+        print(key)
+        print('Accuracy: ', metrics[key]['accuracy'])
+        print('Test loss: ', metrics[key]['test_loss'])
