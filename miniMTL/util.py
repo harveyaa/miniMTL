d64abf133d0bb03d178470ad7b4dd28862e74934
harveyaa <annabelle.ahrv@gmail.com>
2022-03-25 18:21:25 -0400

    added three way split

--- miniMTL/util.py
+++ miniMTL/util.py
@@ -1,12 +1,29 @@
-
+import numpy as np
 from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 
-def split_data(dataset,test_size=0.2,seed=0):
-    train_idx, test_idx, _, _ = train_test_split(range(len(dataset)),
-                                                dataset.Y,
-                                                stratify=dataset.Y,
-                                                test_size=test_size,
-                                                random_state=seed)
+def split_data(dataset,splits=(0.8,0.2),seed=0):
+    assert np.sum(splits) == 1
+    assert (len(splits)==2) | (len(splits)==3)
 
-    return Subset(dataset,train_idx), Subset(dataset,test_idx)
+    if len(splits) == 2:
+        train_idx, test_idx, _, _ = train_test_split(range(len(dataset)),
+                                                    dataset.Y,
+                                                    stratify=dataset.Y,
+                                                    test_size=splits[1],
+                                                    random_state=seed)
+
+        return Subset(dataset,train_idx), Subset(dataset,test_idx)
+    else:
+        train_idx, test_idx, _, _ = train_test_split(range(len(dataset)),
+                                                    dataset.Y,
+                                                    stratify=dataset.Y,
+                                                    test_size=(splits[1]+splits[2]),
+                                                    random_state=seed)
+        
+        test_idx, val_idx, _, _ = train_test_split(test_idx,
+                                                    dataset.Y[test_idx],
+                                                    stratify=dataset.Y[test_idx],
+                                                    test_size=splits[2]/(splits[1] + splits[2]),
+                                                    random_state=seed)
+        return Subset(dataset,train_idx), Subset(dataset,test_idx), Subset(dataset,val_idx)
