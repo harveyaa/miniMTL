bb23aeb7eae23549f765fc650b4eb8d0a655bd3a
Annabelle Harvey <harveyaa@foam.local>
2023-05-24 15:24:48 -0400

    typo

--- miniMTL/mps.py
+++ miniMTL/mps.py
@@ -32,7 +32,7 @@ class MPSModel(nn.Module):
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.to(self.device)
-        print(f'Initialized HPSModel using: {self.device}.\n')
+        print(f'Initialized MPSModel using: {self.device}.\n')
     
     def forward(self,X,task_names):
         """
